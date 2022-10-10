87ae5da6344c55c770d9fb67bb9dfba163063df9
juyige <juyige>
2022-10-10 03:18:40 +0000

    Initial commit

---
+++ src/main/java/com/git_demo/Application.java
@@ -0,0 +1,9 @@
+package com.git_demo;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
